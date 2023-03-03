5ade529b7114dbd0dd7c9b5916e1615190c9cc6c
MWX <37146632+mwx2006@users.noreply.github.com>
2023-03-03 17:11:50 +0800

    First Release

---
+++ controllers/Base62.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace Base62 {
+std::string encode(uint64_t decoding);
+uint64_t decode(std::string encoding);
+const std::string characterSet =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+} // namespace Base62
---
+++ controllers/ShortUrl.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Url.h>
+#include <drogon/HttpController.h>
+
+using namespace drogon;
+using Url = drogon_model::sqlite3::Url;
+
+class ShortUrl : public drogon::HttpController<ShortUrl> {
+
+  std::set<std::string> TLDs;
+  orm::Mapper<Url> mp{app().getDbClient()};
+  HttpResponsePtr root_resp;
+  std::string domain;
+public:
+  METHOD_LIST_BEGIN
+  ADD_METHOD_TO(ShortUrl::generate, "/", Get, Post);
+  ADD_METHOD_TO(ShortUrl::redirect, "/{}", Get);
+  METHOD_LIST_END
+  void generate(const HttpRequestPtr &req,
+                std::function<void(const HttpResponsePtr &)> &&callback);
+  void redirect(const HttpRequestPtr &req,
+                std::function<void(const HttpResponsePtr &)> &&callback,
+                std::string s);
+  ShortUrl();
+};
